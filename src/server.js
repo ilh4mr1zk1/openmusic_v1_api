74a0a0480422e9339593f0ac71435090f1bfda46
ilhamrizki <irp100202@gmail.com>
2023-02-21 22:35:00 +0700

    project dicoding open music v1 api

---
+++ src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  // const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+
+};
+ 
+ 
+init();
